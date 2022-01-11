4ae3dfa9ef75f12deb35ec5df4682766b839fb3d
kk <kk@msm.cc>
2022-01-11 08:53:10 +0700

    add

--- src/AssetsCountry.php
+++ src/AssetsCountry.php
@@ -1,12 +1,14 @@
 <?php
 /*
- * @Author: your name
- * @Date: 2021-09-30 08:21:59
- * @LastEditTime: 2021-09-30 10:15:50
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \aa\api\AssetsCountry.php
+ * @Arthur: kk
+ * @Date: 2022-01-10 17:57:59
+ * @LastEditTime: 2022-01-11 08:52:06
+ * @LastEditors: your name
+ * @Description: 自動生成 [嚴格紀律 Description]
+ * @FilePath: \assets_country\src\AssetsCountry.php
+ * 嚴格紀律
  */
+
 namespace uunmask2000_kk\AssetsCountry;
 
 class AssetsCountry
@@ -15,7 +17,9 @@ class AssetsCountry
      * @var string
      */
     protected $img_path = 'https://raw.githubusercontent.com/uunmask2000/assets_country/main/src/img/';
-    public function __construct() {}
+    public function __construct()
+    {
+    }
     /**
      * 獲取根據國家(地區)縮碼國旗
      *
@@ -69,6 +73,38 @@ class AssetsCountry
             }
 
             return $lists;
+            // //code...
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function getCountryFlagByColumn($key = 'tw', $Column = 'code')
+    {
+        try {
+            $json  = file_get_contents(__DIR__ . '/json/lists.json');
+            $lists = json_decode($json, 1);
+
+            $id = 0;
+            if ($Column == 'code') {
+                $key =                strtoupper($key);
+            } else if ($Column == 'code') {
+                $key =             (int)($key);
+            }
+
+            try {
+                $id = array_search(
+                    $key,
+                    array_column($lists, $Column)
+                );
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+
+            $output = $lists[$id];
+            $output['flag'] =  $this->img_path . strtoupper($output['code']) . '.png';
+
+            return $output ;
             // //code...
         } catch (\Throwable $th) {
             throw $th;
